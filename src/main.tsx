55a3ab026d7af6a575948eb00b60cd3bb0df11eb
Ali Adnan <ali.adnani@hotmail.com>
2022-02-27 17:17:35 +0800

    a

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,14 @@ import { MantineProvider, TypographyStylesProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Main } from "./pages/main";
-import './index.css'
+import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
-
 
 ReactDOM.render(
   <React.StrictMode>
-<ChakraProvider>
-  
-        <Main />
-</ChakraProvider>
+    <ChakraProvider>
+      <Main />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
